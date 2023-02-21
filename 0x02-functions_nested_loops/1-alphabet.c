2cb72a5407b4654c4203b005ea3d83d7f8dfc5ce
NathanMakh <NathanMakh>
2023-02-21 17:18:12 +0000

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main-print the alphabet in lowercase followed by newline
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	
+	while(alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
