d2c84802015f969354dcb9c58c68c649480f834d
NathanMakh <NathanMakh>
2023-02-22 20:22:49 +0000

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * int _isupper - checks for uppercase letter
+ * @c: integer to be checked
+ * Return: 1 if uppercase 0 if lowercase
+ */
+
+int _isupper(int c);
+{
+	int c;
+
+	uppercase = 'A';
+
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
