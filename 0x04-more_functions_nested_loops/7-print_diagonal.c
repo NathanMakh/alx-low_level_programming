74f227a4d2b389ee434c8c3e5faa971884f836b0
NathanMakh <NathanMakh>
2023-02-23 19:25:57 +0000

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonals
+ * @n: number of times diagonal should be printed
+ */
+
+void print_diagonal(int n)
+{
+	int n = times;
+
+	for (times = n; times > 0; times--)
+		_putchar('\\');
+	_putchar('\n');
+}
