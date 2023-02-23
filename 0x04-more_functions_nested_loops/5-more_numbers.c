4361f4349d24a8052f06b52b1b34e11931beb421
NathanMakh <NathanMakh>
2023-02-23 16:13:51 +0000

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the number
+ */
+
+void more_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 14; n++)
+	{
+		_putchar(i);
+		
+		for (n = 0; n <= 14; 10n++)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
