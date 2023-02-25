eab570719be788eb107548e4cab25c7b88833324
NathanMakh <NathanMakh>
2023-02-25 09:00:16 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,18 +14,15 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else
-			printf("%d", i);
+		printf("%d", i);
 
-		if (i < 100)
-			printf(" ");
+		if (i % 3)
+			printf("Fizz");
+		else if (i % 5)
+			printf("Buzz");
+		else if (i % 3 && i % 5)
+			printf("FizzBuzz");
 	}
-	printf("\n");
+	printf('\n');
 	return (0);
 }
