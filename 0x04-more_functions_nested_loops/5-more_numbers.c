25601d334046decf2a9aa9c5f845ae53bf2e3824
NathanMakh <NathanMakh>
2023-02-25 07:43:46 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
 	int n;
 
-	for (n % 10 = 0; n % 10 <= 14; n % 10)
+	for (n % 10 = 0; n % 10 <= 14; n % 10++)
 	{
-		_putchar(n);
+		_putchar(n % 10);
 	}
 	_putchar('\n');
 }
