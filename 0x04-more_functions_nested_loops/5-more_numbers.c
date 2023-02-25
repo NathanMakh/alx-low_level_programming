0c56f7e662a2010e6f279c73ffa0e4519fb5a5d6
NathanMakh <NathanMakh>
2023-02-25 07:52:00 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,16 +2,15 @@
 
 /**
  * more_numbers - print 10 times the number
- * @n: character to be printed
  */
 
 void more_numbers(void)
 {
 	int n;
 
-	for (n % 10 = 0; n % 10 <= 14; n % 10++)
+	for ((n % 10) = 0; (n % 10) <= 14; (n % 10)++)
 	{
-		_putchar(n % 10);
+		_putchar(n);
 	}
 	_putchar('\n');
 }
