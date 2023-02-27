20942dab28863930017a0b419a6978785562d3b3
NathanMakh <NathanMakh>
2023-02-27 16:12:33 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - function to return length of a string
+ *@s: string
+ *Return: length
+ */
+
+int _strlen(char *s)
+{
+	char *s;
+	int len;
+
+	len = _strlen(s);
+	return (len);
+}
