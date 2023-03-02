0c068a13e2db26739cb5f1fadba6245fb02fa239
NathanMakh <NathanMakh>
2023-03-02 12:17:51 +0000

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * char *_strcat - funtion that concatenates two strings
+ * @dest: string to be appended
+ * @src: string to be appended
+ * Return: *dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *dest, *src;
+
+	*_strcat(*src, *dest);
+	return (*dest);
+}
