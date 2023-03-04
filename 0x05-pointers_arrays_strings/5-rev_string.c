16953e66181cc3ad249eee368027b75ebb656cdd
NathanMakh <NathanMakh>
2023-03-04 17:14:14 +0000

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,13 +2,13 @@
 
 /**
  * rev_string - function that reverses a string
+ * @s: string to be reversed
  */
 
 void rev_string(char *s)
 {
 	int i = 0;
 	int j = (strlen(s) - 1);
-	char tmp;
 
 	while (i < j)
 		swap(i, j);
