42d49dc51019ee8cf82ce31eabc00a620a54d07a
NathanMakh <NathanMakh>
2023-03-04 17:29:02 +0000

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,6 +2,7 @@
 
 /**
  * puts2 - function to print every other character
+ *@str: string to be printed
  */
 
 void puts2(char *str)
@@ -9,6 +10,6 @@ void puts2(char *str)
 	char *s;
 	int i;
 
-	for (i = 0; i <= strlen(str); i++)
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
 		_putchar(str[i]);
 }
