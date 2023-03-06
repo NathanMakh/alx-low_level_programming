baf91eee6e1ab7657fdeea0849868517895ca4ae
NathanMakh <NathanMakh>
2023-03-06 12:29:01 +0000

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,39 +3,27 @@
 
 /**
  * _strstr - function to locate and return substring
- * @haystack: string
- * @needle: substring
- * Return: pointer to index of string
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0;
-
-	int j, x;
-
-	if (needle[0] == '\0'
-			return (haystack);
-
-	while (haystack[i] != '\0')
+	for (; *haystack != '\0'; haystack++)
 	{
-		if (haaystack[i] == needle[0])
-		{
-			x = i, j = 0;
-			while (needle[j] != '\0')
-			{
-				if (haystack[x] == needle[j])
-					x++, j++;
+		char *one = haystack;
+		char *two = needle;
 
-				else
-					break;
-			}
-			if (needle[j] == '\0')
-			{
-				return (haystack + i);
-			}
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
 		}
-		i++;
+
+		if (*two == '\0')
+			return (haystack);
 	}
+
 	return (NULL);
 }
